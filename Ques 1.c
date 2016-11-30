0bb69d21fb331d60aeee7ea5ccd5a0301f723223
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-11-30 17:16:16 +0530

    Completed Ques 1

--- Ques 1.c
+++ Ques 1.c
@@ -9,10 +9,27 @@ int fact(unsigned int i){
   return i * fact(i-1);
 }
 
+int fibonacci(int i) {
+
+   if(i == 0) {
+      return 0;
+   }
+
+   if(i == 1) {
+      return 1;
+   }
+   return fibonacci(i-1) + fibonacci(i-2);
+}
+
 int main(int argc, char const *argv[]) {
-  int i;
+  int i ,count = 0;
   printf("Enter number to calculate factorial and generate Fibonacci series.\n");
   scanf("%d",&i);
+
   printf("Factorial is  = %d\n",fact(i));
+  count =i;
+  for (i = 0; i < count; i++) {
+     printf("%d\t", fibonacci(i));
+  }
   return 0;
 }

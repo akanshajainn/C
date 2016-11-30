71bbe283f36e549d8cd688782b25c35049c1c51f
Arjun S <arjun.mi1@iiitmk.ac.in>
2016-11-30 17:49:18 +0530

    main added to ques 3

--- Ques 3.c
+++ Ques 3.c
@@ -1,1 +1,8 @@
-3. Write a program to create a inventory program to accept product name, unit price and quantity and to display the details with actual value of N products using structure and function
+// 3. Write a program to create a inventory program to accept product name, unit price and quantity and to display the details with actual value of N products using structure and function
+
+#include "stdio.h"
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}

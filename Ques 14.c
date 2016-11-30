a25ec19fe2b8d9fd78c1a3f6fc32ad7490dbae43
Akansha Jain <akansha.da1@iiitmk.ac.in>
2016-12-01 00:44:03 +0530

    Still needed to be done

--- Ques 14.c
+++ Ques 14.c
@@ -1,1 +1,13 @@
-14.   Suppose a file contains student’s records with each record containing name and age of a student. Write a program to read these records and display them.
+//14.   Suppose a file contains student’s records with each record containing name and age of a student. Write a program to read these records and display them.
+#include <stdio.h>
+int main()
+{
+    FILE *f1;
+    char ch;
+    f1 = fopen("source.txt", "r");
+    printf("Record of the students :\n\n");
+    while ((ch = fgetc(f1)) != EOF)
+        printf("%c",ch);
+    fclose(f1);
+    return 0;
+}
--- Ques 8.c
+++ Ques 8.c
@@ -1,14 +1,36 @@
-8. Write a program to sort names in alphabetic order using array of pointers.
-#include<stdio.h>
-int main()
-{char i;
+//8. Write a program to sort names in alphabetic order using array of pointers.
+#include <stdio.h>
+//#include<malloc.h>
+#include<string.h>
+#define MAX 20
+int main ()
+{
+   char *names[MAX],*temp=NULL;
+   int i,j,n,flag=1;
+   printf("Enter the number of strings: ");
+   scanf("%d",&n);
+   printf("Enter the strings:\n");
+   for(i=0;i<n;i++)
+   {
+   names[i]=(char*)calloc(MAX,sizeof(char));
+   scanf("%s",&names[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+    flag=0;
+    for(j=0;j<n-i;j++)
+    {
+      if(strcmp(names[j],names[j+1])>0)
+      {
+        temp=names[j];
+          names[j]=names[j+1];
+          names[j+1]=temp;
 
-char *name[10];
-printf("Enter name max upto 10 characters \n");
-max=name[0];
-for(i='a';i<='z',i++)
-if(max<i);
-max=
-
-
+    }
+    }
+   }
+   printf("\nStrings in sorted order:\n");
+   for(j=0;j<n;j++)
+      printf("%s\n",names[j]);
+   return 0;
 }
